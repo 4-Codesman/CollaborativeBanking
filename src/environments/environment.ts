7619949c91dfd91c1215fe27f4d226cd7ccd8377
Ayanda-Bradley-Mzizi <2688042@students.wits.ac.za>
2025-07-25 21:20:35 +0200

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     apiKey: "AIzaSyBalXBEIKgzHo23MIzBpEYIwqTv-Neopsc",
     authDomain: "collaborativebanking.firebaseapp.com",
     projectId: "collaborativebanking",
-    storageBucket: "collaborativebanking.appspot.com", // ✅ FIXED: valid Firebase bucket domain
+    storageBucket: "collaborativebanking.appspot.com", // ✅ fixed
     messagingSenderId: "32856308947",
     appId: "1:32856308947:web:5489604833e896d2632826",
     measurementId: "G-SX92BENWR9"
