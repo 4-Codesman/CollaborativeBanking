bdb69d55c44f8900d4f49ce49c238cf181656ec4
Zayd Fazel-Ellahi <zaydellahi@gmail.com>
2025-07-27 22:58:48 +0200

    Create, Join and View Stokvels creation & CPrototype

---
+++ src/app/pages/stokvel/CreateStokvel/Cstokvel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-Cstokvel', 
+  templateUrl: './Cstokvel.html', 
+  styleUrls: ['./Cstokvel.css'], 
+  standalone: true,
+  imports: [RouterLink]
+})
+
+export class CStokvelComponent {
+}
---
+++ src/app/pages/stokvel/ViewStokvel/Vstokvel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-Vstokvel', 
+  templateUrl: './Vstokvel.html', 
+  styleUrls: ['./Vstokvel.css'], 
+  standalone: true,
+  imports: [RouterLink]
+})
+
+export class VStokvelComponent {
+}
