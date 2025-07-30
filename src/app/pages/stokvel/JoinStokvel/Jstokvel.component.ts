76be92b8006ae48995d48f53e030637db7a4bbab
Zayd Fazel-Ellahi <zaydellahi@gmail.com>
2025-07-31 01:00:10 +0200

    Join Stokvel components made v1

--- src/app/pages/stokvel/JoinStokvel/Jstokvel.component.ts
+++ src/app/pages/stokvel/JoinStokvel/Jstokvel.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-Jstokvel', 
   templateUrl: './Jstokvel.html', 
   styleUrls: ['./Jstokvel.css'], 
   standalone: true,
-  imports: [RouterLink]
+  imports: [RouterLink, CommonModule]
 })
 
-export class JStokvelComponent {
+export class JStokvelComponent implements OnInit {
+  stokvelIds: string[]= [];
+  loading = true;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const email = localStorage.getItem('Email');
+    if (email) {
+      this.http.post<{ stokvels: string[] }>(`${environment.apiUrl}/join/confirm-user`, { email }).subscribe({
+        next: (response) => {
+          this.stokvelIds = response.stokvels;
+          this.loading = false;
+        },
+        error: (err) => {
+          console.error('Error fetching user stokvels:', err);
+          this.stokvelIds = [];
+          this.loading = false;
+        }
+      });
+    } else {
+      this.stokvelIds = [];
+      this.loading = false;
+    }
+  }
 }
