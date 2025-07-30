a08b931226118b938530f42a2846c3142fea8179
Zayd Fazel-Ellahi <zaydellahi@gmail.com>
2025-07-30 03:34:10 +0200

    CStokvel adjusted for emails

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from 'src/environments/environment'; 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  private apiUrl = 'https://4codesmanwebappbe-drbresg4h5cpcjdm.canadacentral-01.azurewebsites.net'; 
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
  postUserLogin(user: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/api/users/login`, user);
+    return this.http.post(`${this.apiUrl}/users/login`, user);
+  }
+
+  getEmails(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.apiUrl}/users/emails`);
+  }
+
+  createStokvel(stokvelData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/stokvels/create`, stokvelData);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,35 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { environment } from './environments/environment';
 
-import {routes} from './app/app.routes';
+import { routes } from './app/app.routes';
 import { provideRouter } from '@angular/router';
+
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material modules to make available globally
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 bootstrapApplication(App, {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),  // ✅ Correct key
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    
-    provideRouter(routes)// ✅ Auth provider
+    provideRouter(routes),
+
+    // Register Material + Animations globally
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      MatAutocompleteModule,
+      MatChipsModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatIconModule
+    )
   ]
 }).catch((err) => console.error(err));
