2c53ec9f68f1af04174469a8d768b98552f70bae
CameronPadiachy <cameronpadiachy@icloud.com>
2025-07-30 18:59:56 +0200

    settings page v1.00

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { Settings } from './pages/settings/settings.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent }, // this is the homepage
+  { path: 'settings', component: Settings }
+
 ];
